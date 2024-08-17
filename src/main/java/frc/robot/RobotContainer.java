a486ef9eef5b9e01a57b3009d2488b4890929a8e
Tyler Mueller <tylerjacobmueller08@gmail.com>
2024-08-17 15:44:25 -0600

    Example Subsystem and Command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,7 +9,10 @@ import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
 import frc.robot.subsystems.VisionSubsystem;
 
@@ -19,6 +22,7 @@ public class RobotContainer {
 
   // Subsystems
   private final SwerveSubsystem drivebase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve"));
+  private final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
   public final VisionSubsystem visionSubsystem = new VisionSubsystem(drivebase);
 
   // Controllers
@@ -48,6 +52,13 @@ public class RobotContainer {
 
   private void configureBindings() {
     driverController.back().onTrue(Commands.runOnce(drivebase::zeroGyro));
+
+    // Schedule `exampleMethodCommand` when the Driver Controller's B button is pressed, cancelling on release.
+    driverController.b().whileTrue(exampleSubsystem.exampleMethodCommand());
+
+    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+    new Trigger(exampleSubsystem::exampleCondition)
+      .onTrue(new ExampleCommand(exampleSubsystem));
   }
 
   public Command getAutonomousCommand() {
