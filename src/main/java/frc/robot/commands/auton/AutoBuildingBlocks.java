e27378bbeab319ff855c3b8967ec86ba751f97f6
TylerMueller08 <tylerjacobmueller08@gmail.com>
2024-12-02 23:13:13 -0700

    Formatting

--- src/main/java/frc/robot/commands/auton/AutoBuildingBlocks.java
+++ src/main/java/frc/robot/commands/auton/AutoBuildingBlocks.java
@@ -9,9 +9,11 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.RobotContainer;
 
-public class AutoBuildingBlocks {
+public class AutoBuildingBlocks
+{
 
-    public static Command resetOdometry(PathPlannerPath choreoPath) {
+    public static Command resetOdometry(PathPlannerPath choreoPath)
+    {
         return RobotContainer.drivebase.runOnce(
             () -> {
                 Pose2d pose = 
@@ -19,7 +21,8 @@ public class AutoBuildingBlocks {
                         .getTrajectory(new ChassisSpeeds(), new Rotation2d())
                         .getInitialTargetHolonomicPose();
                     
-                if (RobotContainer.drivebase.isRedAlliance()) {
+                if (RobotContainer.drivebase.isRedAlliance())
+                {
                     pose = GeometryUtil.flipFieldPose(pose);
                 }
 
--- src/main/java/frc/robot/commands/auton/AutoCommand.java
+++ src/main/java/frc/robot/commands/auton/AutoCommand.java
@@ -5,8 +5,8 @@ import java.util.List;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
-public abstract class AutoCommand extends SequentialCommandGroup {
+public abstract class AutoCommand extends SequentialCommandGroup
+{
     public abstract List<Pose2d> getAllPathPoses();
-
     public abstract Pose2d getStartingPose();
 }
