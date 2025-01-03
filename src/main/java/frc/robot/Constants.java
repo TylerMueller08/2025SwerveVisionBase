e44a9d42352e9b2c7287a41bbe14338d186667f9
TylerMueller08 <tylerjacobmueller08@gmail.com>
2025-01-02 23:22:58 -0700

    RobotConfiguration and ModuleConfiguration Constants for AutonUtils

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -19,10 +19,25 @@ import swervelib.math.Matter;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+  public static final double FIELD_LENGTH = 16.54; // meters
   public static final double ROBOT_MASS = (60) * 0.453592; // 60 lbs
   public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
   public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms spark max velocity lag
   public static final double MAX_SPEED = Units.feetToMeters(14.5); // Maximum speed of the robot in meters per second, used to limit acceleration
+
+  public static final class ModuleConfiguration {
+    public static final double MOI = 6.883;
+    public static final double WHEEL_COF = 1.19;
+    public static final double TRACK_WIDTH = 0.530225;
+    public static final double WHEEL_RADIUS = Units.inchesToMeters(2);
+  }
+
+  public static final class RobotConfiguration {
+    public static final double MAX_DRIVE_VELOCITY = 3.0;
+    public static final double MAX_DRIVE_ACCELERATION = 4.0;
+    public static final double MAX_ANGULAR_VELOCITY = Units.degreesToRadians(540);
+    public static final double MAX_ANGULAR_ACCELERATION = Units.degreesToRadians(720);
+  }
 
   public static final class AutonConstants {
     public static final PIDConstants TRANSLATION_PID = new PIDConstants(0.7, 0, 0);
@@ -30,12 +45,12 @@ public final class Constants {
   }
 
   public static final class DrivebaseConstants {
-    // Hold time on motor brakes when disabled
-    public static final double WHEEL_LOCK_TIME = 10; // seconds
+    // Hold time on motor brakes when disabled, in seconds.
+    public static final double WHEEL_LOCK_TIME = 10;
   }
 
   public static class OperatorConstants {
-    // Joystick Deadband
+    // Joystick Deadband.
     public static final double DEADBAND         = 0.1;
     public static final double LEFT_Y_DEADBAND  = 0.1;
     public static final double RIGHT_X_DEADBAND = 0.1;
--- src/main/java/frc/robot/commands/auton/utils/AutonUtils.java
+++ src/main/java/frc/robot/commands/auton/utils/AutonUtils.java
@@ -10,29 +10,26 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
 import frc.robot.RobotContainer;
 
 public class AutonUtils {
-    /** Field length in meters. */
-    private static final double FIELD_LENGTH = 16.54;
-
     /** Robot Constraints for Pathfinding. */
-    public static final PathConstraints CONSTRAINTS = new PathConstraints(3.0, 4.0, Units.degreesToRadians(540), Units.degreesToRadians(720));
+    public static final PathConstraints CONSTRAINTS = new PathConstraints(Constants.RobotConfiguration.MAX_DRIVE_VELOCITY, Constants.RobotConfiguration.MAX_DRIVE_ACCELERATION, Constants.RobotConfiguration.MAX_ANGULAR_VELOCITY, Constants.RobotConfiguration.MAX_ANGULAR_ACCELERATION);
 
     /** Robot Configuration for generating trajectories. */
     public static RobotConfig robotConfig;
 
     static {
-        ModuleConfig moduleConfig = new ModuleConfig(0.0508, 3.0, 1.19, DCMotor.getKrakenX60(1), 60.0, 1);
+        ModuleConfig moduleConfig = new ModuleConfig(Constants.ModuleConfiguration.WHEEL_RADIUS, Constants.RobotConfiguration.MAX_DRIVE_VELOCITY, Constants.ModuleConfiguration.WHEEL_COF, DCMotor.getKrakenX60(1), 60.0, 1);
 
         try {
             robotConfig = RobotConfig.fromGUISettings();
         } catch (Exception e) {
             System.err.println("Error loading robot configuration from GUI settings. Using default values.");
             e.printStackTrace();
-            robotConfig = new RobotConfig(27.2155, 6.883, moduleConfig, 0.530225);
+            robotConfig = new RobotConfig(Constants.ROBOT_MASS, Constants.ModuleConfiguration.MOI, moduleConfig, Constants.ModuleConfiguration.TRACK_WIDTH);
         }
     }
     
@@ -52,9 +49,9 @@ public class AutonUtils {
     }
 
     /**
-     * Load the PathPlanner trajectory file to path
-     * @param pathName Name of the path
-     * @return PathPlanner Path
+     * Load the PathPlanner trajectory file to path.
+     * @param pathName Name of the path.
+     * @return PathPlanner Path.
      */
     public static PathPlannerPath loadPath(String pathName) {
         try {
@@ -64,35 +61,32 @@ public class AutonUtils {
             e.printStackTrace();
         }
 
-        // Return null or provide default fallback PathPlannerPath
+        // Return null or provide default fallback PathPlannerPath.
         return null;
     }
 
     /**
-     * Flip a field position to the other side of the field, maintaining a blue alliance origin
-     * 
-     * @param position The position to flip
-     * @return The flipped position
+     * Flip a field position to the other side of the field, maintaining a blue alliance origin.
+     * @param position The position to flip.
+     * @return The flipped position.
      */
     private static Translation2d flipFieldPosition(Translation2d position) {
-        return new Translation2d(FIELD_LENGTH - position.getX(), position.getY());
+        return new Translation2d(Constants.FIELD_LENGTH - position.getX(), position.getY());
     }
 
     /**
-     * Flip a field rotation to the other side of the field, maintaining a blue alliance origin
-     * 
-     * @param rotation The rotation to flip
-     * @return The flipped rotation
+     * Flip a field rotation to the other side of the field, maintaining a blue alliance origin.
+     * @param rotation The rotation to flip.
+     * @return The flipped rotation.
      */
     private static Rotation2d flipFieldRotation(Rotation2d rotation) {
         return new Rotation2d(Math.PI).minus(rotation);
     }
     
     /**
-     * Flip a field pose to the other side of the field, maintaining a blue alliance origin
-     * 
-     * @param pose The pose to flip
-     * @return The flipped pose
+     * Flip a field pose to the other side of the field, maintaining a blue alliance origin.
+     * @param pose The pose to flip.
+     * @return The flipped pose.
      */
     private static Pose2d flipFieldPose(Pose2d pose) {
         return new Pose2d(flipFieldPosition(pose.getTranslation()), flipFieldRotation(pose.getRotation()));
